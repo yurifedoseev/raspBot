721f7250eae63df89082840cb6db516131b24890
Yuri Fedoseev <fedoseev.yuri@gmail.com>
2015-12-05 14:34:44 +0300

    serializer

--- src/main/java/com/raspbot/botapi/client/TelegramClient.java
+++ src/main/java/com/raspbot/botapi/client/TelegramClient.java
@@ -1,9 +1,12 @@
 package com.raspbot.botapi.client;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.ObjectMapper;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import com.raspbot.botapi.models.Update;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +20,30 @@ public class TelegramClient {
     }
 
     public List<Update> getUpdates() throws UnirestException {
+
+        Unirest.setObjectMapper(new ObjectMapper() {
+
+            private com.fasterxml.jackson.databind.ObjectMapper mapper
+                    = new com.fasterxml.jackson.databind.ObjectMapper();
+
+            public <T> T readValue(String value, Class<T> valueType) {
+
+                try {
+                    return mapper.readValue(value, valueType);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+
+            public String writeValue(Object o) {
+                try {
+                    return mapper.writeValueAsString(o);
+                } catch (JsonProcessingException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
         HttpResponse<String> response = Unirest.get(baseUrl + "getUpdates").asString();
 
         System.out.println(response.getBody());
